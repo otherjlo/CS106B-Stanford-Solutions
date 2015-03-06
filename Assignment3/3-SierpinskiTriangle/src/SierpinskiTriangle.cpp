d7f57580b0ca89b4015713a912c5c9e3de577ba3
Josue Lopez <josue_lopez@prodigy.net>
2015-03-06 16:16:22 -0600

    Added Sierpinski triangle
    Massive help from another github user.
    Checkout https://github.com/br1992

--- Assignment3/3-SierpinskiTriangle/src/SierpinskiTriangle.cpp
+++ Assignment3/3-SierpinskiTriangle/src/SierpinskiTriangle.cpp
@@ -1,21 +1,56 @@
-/*
- * File: SierpinskiTriangle.cpp
- * ----------------------------
- * Name: [TODO: enter name here]
- * Section: [TODO: enter section leader here]
- * This file is the starter code for the Sierpinski Triangle problem from
- * Assignment #3.
- * [TODO: extend the documentation]
- */
-
 #include <iostream>
+#include <cmath>
 #include "gwindow.h"
+#include "simpio.h"
 using namespace std;
 
+/*Function Prototypes */
+void sierpinskiTriangleWrapper(GWindow &canvas);
+void drawSierpinskiTriangle(double x, double y, double length, int order, GWindow &canvas);
+void drawTriangle(double x, double y, double sidelength, GWindow &canvas);
 /* Main program */
 
+const int WINDOW_WIDTH = 600;
+const int WINDOW_HEIGHT = 500;
+const double M_PI = 3.14;
+
+/*
+ * Major help from br1992
+ * Page: https://github.com/br1992
+ */
 int main() {
-   GWindow gw(600, 500);
-   // [TODO: Fill in the necessary code here]
+   GWindow gw(WINDOW_WIDTH, WINDOW_HEIGHT);
+   sierpinskiTriangleWrapper(gw);
+
+
    return 0;
+}
+
+void sierpinskiTriangleWrapper(GWindow &canvas) {
+    while(true) {
+        int length = getInteger("Enter edge length: ");
+        int order = getInteger("Enter fractal order: ");
+        canvas.clear();
+        double height = sin(M_PI * .6666) * length;
+        drawSierpinskiTriangle(WINDOW_WIDTH/2, WINDOW_HEIGHT/2 - height/2, length, order, canvas);
+    }
+}
+
+void drawSierpinskiTriangle(double x, double y, double length, int order, GWindow &canvas) {
+    //Final order, draw triangle
+    if(order == 0) {
+        drawTriangle(x, y, length, canvas);
+        return;
+    }
+    double height = sin(M_PI * .6666) * length;
+    drawSierpinskiTriangle(x - length/4, y + height/2, length/2, order - 1, canvas); // bottom left triangle
+    drawSierpinskiTriangle(x + length/4, y + height/2, length/2, order - 1, canvas); // bottom right
+    drawSierpinskiTriangle(x,y, length/2, order - 1, canvas); //top middle triangle
+}
+
+void drawTriangle(double x, double y, double sidelength, GWindow &canvas) {
+    double trigHeight = sin(M_PI * .6666) * sidelength;
+    canvas.drawLine(x, y, x + sidelength/2, y + trigHeight); //draw right line
+    canvas.drawLine(x, y, x - sidelength/2, y + trigHeight); //draw left line
+    canvas.drawLine(x - sidelength/2, y + trigHeight, x + sidelength/2, y + trigHeight); // draw bottom line
 }
