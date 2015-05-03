a8e0cb2000b3b41496ac9e86e162e2beb1c58837
Josue Lopez <josue_lopez@prodigy.net>
2015-05-02 22:06:25 -0500

    Commented all public functions on Boggle.h
    All public Boggle functions now commented

--- Assignment4/src/Boggle.h
+++ Assignment4/src/Boggle.h
@@ -16,6 +16,7 @@
 #include "vector.h"
 using namespace std;
 
+/** struct containing row and column of each letter on boggle board */
 typedef struct dieLocation {
     int row, column;
     bool operator <(const dieLocation &d2) const {
@@ -25,15 +26,69 @@ typedef struct dieLocation {
 
 class Boggle {
 public:
+    /**
+     * @brief Boggle Creates an instance of Boggle
+     * @param dictionary The dictionary boggle will use to find words, and check if they exist.
+     * @param boardText The string to represent a Boggle board, "" creates a random board.
+     */
     Boggle(Lexicon& dictionary, string boardText);
+
+    /**
+     * @brief getLetter Returns the letter at the given row and column, if within bounds.
+     * @param row The row to get letter
+     * @param col Column to get letter
+     * @return Letter at given row and column
+     */
     char getLetter(int row, int col);
+
+    /**
+     * @brief checkWord Checks if valid word
+     * @param word Word to be checked
+     * @return If player has not used word, and word is in dictionary, and word is 4 letters or longer
+     */
     bool checkWord(string word);
+
+    /**
+     * @brief humanWordSearch Finds whether or not a word exists on board
+     * @param word The word to be searched for
+     * @return Whether word can be made on board
+     */
     bool humanWordSearch(string word);
+
+    /**
+     * @brief humanScore Player score
+     * @return Player score
+     */
     int humanScore();
+
+    /**
+     * @brief computerWordSearch find all words that player has not found
+     * @return Set of all words computer found
+     */
     Set<string> computerWordSearch();
+
+    /**
+     * @brief getScoreComputer Gets the computer's score
+     * @return The computer's score
+     */
     int getScoreComputer();
+
+    /**
+     * @brief boardSize Rows  * columns
+     * @return Num of die on board
+     */
     int boardSize();
+
+    /**
+     * @brief getLastHighlighted Return last word that was guessed, empty if guess was wrong
+     * @return Last guessed word
+     */
     Queue<dieLocation> getLastHighlighted();
+
+    /**
+     * @brief getPlayerWords All words found by player
+     * @return All words found by player
+     */
     Set<string> getPlayerWords();
 
     // TODO: add any other member functions/variables necessary
